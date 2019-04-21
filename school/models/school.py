bc4cbe234b7dbe0b732d53347bcef2d578edd290
ridho <alfatihridhont@gmail.com>
2019-04-21 14:37:06 +0700

    [bug-fixing]

--- school/models/school.py
+++ school/models/school.py
@@ -7,16 +7,16 @@ class students(models.Model):
     _description =  "this is a student data"
     
     name             = fields.Char(string = "Students name")
-    major            = fields.Char(string = "Major")
+    # major            = fields.Char(string = "Major")
     students_number  = fields.Integer(string = "Students Number")
-    mentor           = fields.Many2one("teachers.data", string = "Mentor")
+    mentor_id        = fields.Many2one("teachers.data", string = "Mentor")
     class_rooms      = fields.Many2many("class.room","students_data_rel","student_id","class_id", string ="Class")
     
 class teacher(models.Model):
     _name = "teachers.data"
     name            = fields.Char(string = "Teachers Name")
     teachers_number = fields.Integer(string = "Teachers Number")
-    students        = fields.One2many("students.data", "mentor", string = "Student ID")
+    students        = fields.One2many("students.data", "mentor_id", string = "Student ID")
     
 class classroom(models.Model):
     _name = "class.room"
